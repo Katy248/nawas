16bd73723da2d3cae3c535fd9154f34194a15be5
ForesLov <fores.clerc@gmail.com>
2023-11-04 21:10:43 +0300

    Music

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
 const player = document.querySelector("#musicplayer");
+tracklist = [
+  "./music/cyberbulling.mp3",
+  "./music/miss_you.mp3",
+  "./music/nine_eleven.mp3",
+];
+player2 = new Audio(tracklist[Math.floor(Math.random() * tracklist.length)]);
+player2.autoplay = true;
+player.src = tracklist[Math.floor(Math.random() * tracklist.length)];
+console.log(player2);
 player.autoplay = true;
 console.log(player);
 player.addEventListener("canplay", () => {
