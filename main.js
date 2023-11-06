087e96c6d1870f88cf25f0e058ca816c35284dd7
ForesLov <fores.clerc@gmail.com>
2023-11-06 17:49:55 +0300

    Мелкие исправления

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
-const player = document.querySelector("#musicplayer");
-tracklist = [
-  "./music/cyberbulling.mp3",
-  "./music/miss_you.mp3",
-  "./music/nine_eleven.mp3",
-];
-player2 = new Audio(tracklist[Math.floor(Math.random() * tracklist.length)]);
-player2.autoplay = true;
-player.src = tracklist[Math.floor(Math.random() * tracklist.length)];
-console.log(player2);
-player.autoplay = true;
-console.log(player);
-player.addEventListener("canplay", () => {
-  player.play();
+document.addEventListener("onload", () => {
+  console.log("documet loaded");
+  const player = document.querySelector("#musicplayer");
+  tracklist = [
+    "./music/cyberbulling.mp3",
+    "./music/miss_you.mp3",
+    "./music/nine_eleven.mp3",
+  ];
+  player2 = new Audio(tracklist[Math.floor(Math.random() * tracklist.length)]);
+  player2.autoplay = true;
+  player.src = tracklist[Math.floor(Math.random() * tracklist.length)];
+  player.autoplay = true;
+  console.log(player);
+  console.log(player2);
+  player.addEventListener("canplay", () => {
+    console.log("can play event");
+    player.play();
+    player2.play();
+  });
 });
