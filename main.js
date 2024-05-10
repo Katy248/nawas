7e67c949744f842091af67f802df3eb1b974cbf0
Katy248 <petrovanton247@gmail.com>
2024-05-10 17:36:45 +0300

    Remove ability to disable cookie notification

--- main.js
+++ main.js
@@ -2,26 +2,18 @@ const trackList = ["./music/cyberbulling.mp3", "./music/miss_you.mp3", "./music/
 const source = trackList[Math.floor(Math.random() * trackList.length)];
 const player = new Audio(source);
 
-const DISABLE_MUSIC_KEY = "disableMusic";
-
 const acceptCookieButton = document.getElementById("cookie-btn");
 const rejectButton = document.getElementById("reject-btn");
 const cookieContainer = document.getElementById("cookie-container");
-const cookieAlert = document.getElementById("cookie-alert");
 
 acceptCookieButton.addEventListener("click", () => {
     cookieContainer.remove();
     tryPlaySound();
 });
 rejectButton.addEventListener("click", () => {
-    localStorage.setItem(DISABLE_MUSIC_KEY, true);
     cookieContainer.remove();
     tryStopSound();
 });
-
-if (!localStorage.getItem(DISABLE_MUSIC_KEY)) {
-    cookieAlert.classList.remove("hidden");
-}
 
 const tryPlaySound = () => {
     try {
